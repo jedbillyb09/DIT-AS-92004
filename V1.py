eb94783ae57287123bed74c4fcf9d562ff983a5a
jedbillyb <jedblenkhorn@gmail.com>
2025-03-27 12:12:51 +1300

    Added meal selction

--- V1.py
+++ V1.py
@@ -3,10 +3,21 @@ name = input("Enter your name: ")
 age = input("Enter your age: ")
 
 #Choose an activity list input
-activity_list = ['Music Jam Session (2 hours, easy, $5 fee)','Science Experiments Lab (3 hours, moderate, $10 fee)','Sports Leadership Training (4 hours, challenging, $12 fee)']
+activity_list = ['Music Jam SessioJn (2 hours, easy, $5 fee)','Science Experiments Lab (3 hours, moderate, $10 fee)','Sports Leadership Training (4 hours, challenging, $12 fee)']
 
 print('Choose an activity: ')
 print(f'1. {activity_list[0]}')
 print(f'2. {activity_list[1]}')
 print(f'3. {activity_list[2]}')
 chosen_activity = input("Enter the number of your chosen activity: ")
+
+#Meal options list input
+meal_options = ['Standerd', 'vegeterian', 'Dairy-free', 'Gluten-free','No meal']
+
+print('Choose a meal option: ')
+print(f'1. {meal_options[0]}')
+print(f'2. {meal_options[1]}')
+print(f'3. {meal_options[2]}')
+print(f'4. {meal_options[3]}')
+chosent_meal = input("Enter the number of your chosen meal: ")
+
