644a5c3a20c0db948701e518fff969a571db6c1c
jedbillyb <jedblenkhorn@gmail.com>
2025-03-27 12:05:14 +1300

    Add activity selection

--- V1.py
+++ V1.py
@@ -1,3 +1,12 @@
 #Name and Age inputs
 name = input("Enter your name: ")
 age = input("Enter your age: ")
+
+#Choose an activity list input
+activity_list = ['Music Jam Session (2 hours, easy, $5 fee)','Science Experiments Lab (3 hours, moderate, $10 fee)','Sports Leadership Training (4 hours, challenging, $12 fee)']
+
+print('Choose an activity: ')
+print(f'1. {activity_list[0]}')
+print(f'2. {activity_list[1]}')
+print(f'3. {activity_list[2]}')
+chosen_activity = input("Enter the number of your chosen activity: ")
