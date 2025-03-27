7355bc3394f2ec02fae491525987996a207b8b78
jedbillyb <jedblenkhorn@gmail.com>
2025-03-27 14:35:17 +1300

    Added Validation of activitys, meals, age and name.

--- V1.py
+++ V1.py
@@ -1,6 +1,20 @@
-#Name and Age inputs
-name = input("Enter your name: ")
-age = input("Enter your age: ")
+#Name input wiht validation
+while True:
+    name = input("Enter your name: ")
+    if len(name) > 0:
+        break
+    else:
+        print("Invalid input. Please enter a your name.")
+
+
+#Age input with validation
+while True:
+    age = input(f"Hi {name} please enter your age: ")
+    if len(age) > 0:
+        break
+    else:
+        print("Invalid input. Please enter your age.")
+
 
 #Choose an activity list input
 activity_list = ['Music Jam Session (2 hours, easy, $5 fee)','Science Experiments Lab (3 hours, moderate, $10 fee)','Sports Leadership Training (4 hours, challenging, $12 fee)']
@@ -9,7 +23,15 @@ print('Choose an activity: ')
 print(f'1. {activity_list[0]}')
 print(f'2. {activity_list[1]}')
 print(f'3. {activity_list[2]}')
-chosen_activity = input("Enter the number of your chosen activity: ")
+
+
+# Validating Activity input
+while True:
+    chosen_activity = input("Enter the number of your chosen activity: ")
+    if chosen_activity == ['1', '2', '3']:
+        break
+    else:
+        print("Invalid input. Please enter a number between 1 and 3.")
 
 #Chosen activity fee
 if chosen_activity == '1':
@@ -30,6 +52,14 @@ print(f'3. {meal_options[2]}')
 print(f'4. {meal_options[3]}')
 chosen_meal = input("Enter the number of your chosen meal: ")
 
+# Validating Meal input
+while True:
+    chosen_meal = input("Enter the number of your chosen meal: ")
+    if chosen_meal == ['1', '2', '3','4']:
+        break
+    else:
+        print("Invalid input. Please enter a number between 1 and 4.")
+
 #Chosen meal fee
 if chosen_meal == '1' or chosen_meal == '2' or chosen_meal == '3':
     meal_fee = 7
@@ -38,3 +68,6 @@ else:
 
 #Total fee
 total_fee = activity_fee + meal_fee
+
+#output
+print(f'{name}, aged {age}, has chosen {chosen_activity}, meal option: {chosen_meal} The total  fee is $ {total_fee}')
